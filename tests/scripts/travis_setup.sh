63b5cad1f500c0ceca316322430636e95b36392f
qadan <daitken@discoverygarden.ca>
2014-10-23 21:35:37 -0300

    forgot the script

---
+++ tests/scripts/travis_setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+mysql -u root -e 'create database drupal;'
+mysql -u root -e "create database fedora;"
+mysql -u root -e "GRANT ALL PRIVILEGES ON fedora.* To 'fedora'@'localhost' IDENTIFIED BY 'fedora';"
+mysql -u root -e "GRANT ALL PRIVILEGES ON drupal.* To 'drupal'@'localhost' IDENTIFIED BY 'drupal';"
+cd $HOME
+pear channel-discover pear.drush.org
+pear upgrade --force Console_Getopt
+pear upgrade --force pear
+pear channel-discover pear.drush.org
+
+wget http://alpha.library.yorku.ca/drush-6.3.tar.gz
+tar xf drush-6.3.tar.gz
+sudo mv drush-6.3 /opt/
+sudo ln -s /opt/drush-6.3/drush /usr/bin/drush
+
+wget http://alpha.library.yorku.ca/PHP_CodeSniffer-1.5.4.tgz
+pear install PHP_CodeSniffer-1.5.4.tgz
+
+wget http://alpha.library.yorku.ca/phpcpd.phar
+sudo mv phpcpd.phar /usr/local/bin/phpcpd
+sudo chmod +x /usr/local/bin/phpcpd
+
+phpenv rehash
+drush dl --yes drupal
+cd drupal-*
+drush si minimal --db-url=mysql://drupal:drupal@localhost/drupal --yes
+drush runserver --php-cgi=$HOME/.phpenv/shims/php-cgi localhost:8081 &>/tmp/drush_webserver.log &
+drush dl --yes coder
+drush dl --yes potx-7.x-1.0
+drush en --yes coder_review
+drush en --yes simpletest
+drush en --yes potx
+drush cc all
+# The shebang in this file is a bogeyman that is haunting the web test cases.
+rm /home/travis/.phpenv/rbenv.d/exec/hhvm-switcher.bash
+sleep 20
